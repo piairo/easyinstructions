ccc5f73efdef7ad0e9579982499b07d16de4fe11
Pierre M'baga <pierrembaga@gmail.com>
2019-08-28 21:10:26 +0100

    changedemoji check-in

--- app/javascript/plugins/init_copy_to_clipboard.js
+++ app/javascript/plugins/init_copy_to_clipboard.js
@@ -1,28 +1,28 @@
-import { addFlashMessage } from './init_flash_messages';
+// import { addFlashMessage } from './init_flash_messages';
 
-const initCopyToClipboard = () => {
-  const inputCopy = document.querySelectorAll('.inputCopy');
-  // const buttonCopy = document.querySelector('.btnCopy')
-  inputCopy.forEach((input) => {
-    input.addEventListener("click", () => {
-      copy(input);
-    })
-  })
+// const initCopyToClipboard = () => {
+//   const inputCopy = document.querySelectorAll('.inputCopy');
+//   // const buttonCopy = document.querySelector('.btnCopy')
+//   inputCopy.forEach((input) => {
+//     input.addEventListener("click", () => {
+//       copy(input);
+//     })
+//   })
 
-  const btnCopy = document.querySelectorAll('.btn-copy');
-  btnCopy.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const input = btn.parentNode.parentNode.querySelector('.inputCopy');
-      copy(input);
-    })
-  })
-};
+//   const btnCopy = document.querySelectorAll('.btn-copy');
+//   btnCopy.forEach((btn) => {
+//     btn.addEventListener("click", () => {
+//       const input = btn.parentNode.parentNode.querySelector('.inputCopy');
+//       copy(input);
+//     })
+//   })
+// };
 
-const copy = (inputNode) => {
-  inputNode.select();
-  document.execCommand('copy');
-  window.getSelection().removeAllRanges();
-  addFlashMessage('Copied', 'primary');
-}
+// const copy = (inputNode) => {
+//   inputNode.select();
+//   document.execCommand('copy');
+//   window.getSelection().removeAllRanges();
+//   addFlashMessage('Copied', 'primary');
+// }
 
-export { initCopyToClipboard };
+// export { initCopyToClipboard };
