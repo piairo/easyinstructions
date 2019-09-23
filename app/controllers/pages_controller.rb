92b69806bd9cfbfee21915a24cb6881382973524
Pierre M'baga <pierrembaga@gmail.com>
2019-09-23 15:32:31 +0100

    new Page & route for #homes_for_booking

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,22 @@
 
 class PagesController < ApplicationController
   def contact
+
   end
 
   def about
   end
 
   def home
+
+  end
+
+ def homes_for_booking
+    @flat = Flat.where(name: params[:flat_name])[0]
   end
 
 end
+
+ def arrival_params
+    params.require(:flat).permit(:name)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :flats do
-  resources :arrivals
+    resources :arrivals
   end
   # get 'arrival/:number', to: 'arrivals#show', as: :arrival
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
   get 'sms', to: 'arrivals#send_sms', as: :sms
   get 'keyinfo/:flat_name', to: 'keyinfos#show', as: :keyinfo
+  get ':flat_name', to: 'pages#homes_for_booking'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
