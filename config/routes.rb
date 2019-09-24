9324653e0cd42d91a8de5fed8f3330465f2e0378
Pierre M'baga <pierrembaga@gmail.com>
2019-09-24 21:49:10 +0100

    tweak routes - importance of order v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,36 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  #arrivals based on number (not ID like in resources)
-  get 'flats/:flat_id/arrivals/:number/edit', to: 'arrivals#edit', as: :edit_flat_arrival
-  get 'flats/:flat_id/arrivals/:number', to: 'arrivals#show', as: :flat_arrival
-  # put 'flats/:flat_id/arrivals/:number', to: 'arrivals#update' (WORK ONLY WITH :ID)
-  delete 'flats/:flat_id/arrivals/:number', to: 'arrivals#destroy'
+  resources :flats
 
-   resources :flats do
-    resources :arrivals, only: [:index, :create, :new, :update]
-  end
+    #arrivals based on number (not ID like in resources)
+    get 'flats/:flat_id/arrivals', to: 'arrivals#index', as: :flat_arrivals
+    post 'flats/:flat_id/arrivals', to: 'arrivals#create'
+    get 'flats/:flat_id/arrivals/new', to: 'arrivals#new', as: :new_flat_arrival
+    get 'flats/:flat_id/arrivals/:number/edit', to: 'arrivals#edit', as: :edit_flat_arrival
+    get 'flats/:flat_id/arrivals/:number', to: 'arrivals#show', as: :flat_arrival
+    # attention for update :number does not work but only :id
+    patch 'flats/:flat_id/arrivals/:id', to: 'arrivals#update'
+    put 'flats/:flat_id/arrivals/:id', to: 'arrivals#update'
+    delete 'flats/:flat_id/arrivals/:number', to: 'arrivals#destroy'
 
- # perso
-  get 'about', to: 'pages#about', as: :about
-  get 'contact', to: 'pages#contact', as: :contact
-  get 'sms', to: 'arrivals#send_sms', as: :sms
-  get 'keyinfos/:flat_name', to: 'keyinfos#show', as: :keyinfos
-  get ':flat_name', to: 'pages#homes_for_booking', as: :homes_for_booking
-  root to: 'pages#home'
+
+   # perso
+    get 'about', to: 'pages#about', as: :about
+    get 'contact', to: 'pages#contact', as: :contact
+    get 'sms', to: 'arrivals#send_sms', as: :sms
+    get 'keyinfos/:flat_name', to: 'keyinfos#show', as: :keyinfos
+    get ':flat_name', to: 'pages#homes_for_booking', as: :homes_for_booking
+    root to: 'pages#home'
+
 end
 
   # my verif of arrivals based on number before resources(:id) VS after (:number)
-                         #POST   /flats/:flat_id/arrivals(.:format)          arrivals#create
-       #edit_flat_arrival GET    /flats/:flat_id/arrivals/:id/edit(.:format) arrivals#edit
-            #flat_arrival GET    /flats/:flat_id/arrivals/:id(.:format)      arrivals#show
+          #flat_arrivals GET    /flats/:flat_id/arrivals(.:format)          arrivals#index
+                        #POST   /flats/:flat_id/arrivals(.:format)          arrivals#create
+       #new_flat_arrival GET    /flats/:flat_id/arrivals/new(.:format)      arrivals#new
+      #edit_flat_arrival #GET    /flats/:flat_id/arrivals/:id/edit(.:format) arrivals#edit
+           #flat_arrival #GET    /flats/:flat_id/arrivals/:id(.:format)      arrivals#show
                          #PATCH  /flats/:flat_id/arrivals/:id(.:format)      arrivals#update
                          #PUT    /flats/:flat_id/arrivals/:id(.:format)      arrivals#update
                          #DELETE /flats/:flat_id/arrivals/:id(.:format)      arrivals#destroy
