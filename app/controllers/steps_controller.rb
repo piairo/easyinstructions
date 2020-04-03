7e09cb329bbe1b622fd2390c80e34145068d7879
Pierre M'baga <pierrembaga@gmail.com>
2020-04-03 11:15:32 +0100

    twilio security fix

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -10,8 +10,11 @@ class StepsController < ApplicationController
   def send_sms
   @location = request.location.city
 
+  # Get your Account SID and Auth Token from twilio.com/console
+  account_sid = ENV['TWILIO_ACCOUNT_SID']
+  auth_token = ENV['TWILIO_AUTH_TOKEN']
   # set up a client to talk to the Twilio REST API
-  @client = Twilio::REST::Client.new twilio_account_sid, twilio_auth_token
+  @client = Twilio::REST::Client.new(account_sid, auth_token)
 
   @client.api.account.messages.create(
   from: '+441670432062',
